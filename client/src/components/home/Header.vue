070d90164085b475b7834322bb9146b150a0baf6
ravenusmc <mcuddy77@gmail.com>
2019-07-27 19:24:11 -0400

    working on header for home page

---
+++ client/src/components/home/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Header</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="home">
+  <div>
+    <Header/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Header from '@/components/home/Header.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    Header,
   },
 };
 </script>
