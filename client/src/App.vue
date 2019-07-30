3c4d9ab8ee4e1c8b5ba1a03a027b5ce1cf93c203
ravenusmc <mcuddy77@gmail.com>
2019-07-30 16:57:35 -0400

    added navbar for navigation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
+
+    <Navbar/>
+
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
+
     <router-view/>
+
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar';
+
+export default {
+  components: {
+    Navbar,
+  },
+}
+</script>
 
 <style scoped>
 </style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+           <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/About">About</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      drawer: false,
+      links: [
+        {icon: 'dashboard', text: 'Home', route: '/'},
+        {icon: 'folder', text: 'About Me', route: '/about'},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
