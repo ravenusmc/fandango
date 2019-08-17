749dcd7bf922754fd0e1e4308468e48cfc5957da
ravenusmc <mcuddy77@gmail.com>
2019-08-17 19:06:07 -0400

    working on graph component

--- client/src/components/home/GraphArea.vue
+++ client/src/components/home/GraphArea.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    Graphs
+    <section>
+      Graphs
+    </section>
   </div>
 </template>
 
@@ -11,4 +13,11 @@ export default {
 </script>
 
 <style scoped>
+  section {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    border: 2px solid black;
+    margin-left: 10%;
+    margin-right: 10%;
+  }
 </style>
